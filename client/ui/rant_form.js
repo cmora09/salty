04449aa29bd61019312c1225bfe0629dda63aee3
Christopher Mora <cmora09@gmail.com>
2016-04-28 22:53:20 -0400

    Added rant text area and disable character count.

--- client/ui/rant_form.js
+++ client/ui/rant_form.js
@@ -1,3 +1,42 @@
-Template.rantForm.onRendered(function (){
-	Session.set('charNums', 0);
-})
+//sets character count to 0 when the form is rendered
+Template.rantForm.onRendered(function () {  
+  Session.set('charNum', 0);
+});
+
+Template.rantForm.events({
+	//grabs current value of the text area.
+	'input .rantBox': function() {
+		Session.set('charNum', $('.rantBox').val().length);
+	},
+
+	'click .rant-button': function() {
+		var rant = $('.rantBox').val();
+		$('.rantBox').val("");
+		Session.set('charNum', 0);
+		Rants.insert({message: rant});
+	}
+});
+Template.rantForm.helpers({
+	//gets current 'charNum' and subtracts it from 200 to show how many characters are left for the rant
+	charCount: function() {
+		return 175 - Session.get('charNum');
+	},
+	//returns different color text is the character count exceeds 200
+	charClass: function() {
+		if (Session.get('charNum') > 175){
+			return 'errCharCount'; //css class name
+		}
+		else{
+			return 'charCount'; //css class name
+		}
+	},
+	//disables button if character number is 0 or less or if the charnum is greater than 200
+	disableButton: function() {
+		if (Session.get('charNum') <= 0 || Session.get('charNum') > 175){
+			return 'disabled';
+		}
+	}
+});
+
+
+
